cd226bb45666739c8dae9b17a04bfd03827db394
Carmen McKie <35529232+carmenmckie@users.noreply.github.com>
2021-11-02 21:06:56 +0000

    Player.cs to store number of stars per player

---
+++ 3D_Mobile_With_Git/Assets/Scripts/Player.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+// To store the number of stars a player has 
+// Time played? 
+public class Player : MonoBehaviour
+{
+    // Start with 0 
+    // If a game is completed, a star is gained 
+    public int numberOfStars = 0; 
+}
