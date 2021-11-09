c2ddf1e127cc5e4eb1339433a63f34cb5646d324
Carmen McKie <35529232+carmenmckie@users.noreply.github.com>
2021-11-09 14:58:32 +0000

    CSV File added to Project

---
+++ 3D_Mobile_With_Git/Assets/Scripts/LoadCSV.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Class to load PGInfo.csv (which will contain the parental / guardian info to adjust 
+// user settings and set goals for stars)
+public class LoadCSV : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
