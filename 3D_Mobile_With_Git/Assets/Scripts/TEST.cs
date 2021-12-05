67a1409b8859efa05f311d67acdf314d22b47041
Carmen McKie <35529232+carmenmckie@users.noreply.github.com>
2021-12-05 12:54:59 +0000

    CAPTCHAGenerator reverted to original version

--- 3D_Mobile_With_Git/Assets/Scripts/TEST.cs
+++ 3D_Mobile_With_Git/Assets/Scripts/TEST.cs
@@ -1,60 +1,60 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI; 
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI; 
 
-// Attached to TestPanel object to test behaviour by printng Debug.Log() statements
+// // Attached to TestPanel object to test behaviour by printng Debug.Log() statements
 
-// Class that was purely used to test different functionalities. 
-public class TEST
-{
-    // public Text textField; 
-    // public string testText = "Initial test"; 
+// // Class that was purely used to test different functionalities. 
+// public class TEST
+// {
+//     // public Text textField; 
+//     // public string testText = "Initial test"; 
 
-    // HandleCSV csvHandling = new HandleCSV(); 
-
-
-    // void Start()
-    // {
-    // }
-
-    //     Debug.Log("count of HandleCSV's internal list ... " + csvHandling.returnCurrentCSV().Count);
-    // {
-    // // Tetsing CSV handling     
-    //     // csvHandling.appendToCSV(); 
-    //     // textField.text = testText;
-    //     List<CSVInfo> readInInfo = csvHandling.readCSV(); 
-    //     // string combindedString = string.Join( ",", readInInfo);
-    //     // testText.Equals(combindedString); 
-    //     // textField.text = testText; 
-    //      foreach (CSVInfo z in readInInfo){
-    //         testText = z.printCSVInfo(); 
-    //     }
-    //     textField.text = testText; 
-
-    //     // Debug.Log("TEST" + readInInfo.Count); 
-    //     CSVInfo test = new CSVInfo("25"); 
-    //     Debug.Log(test.printCSVInfo()); 
-
-    //     Debug.Log("FROM TEST.CS " + HashClass.toSHA256("1111"));
+//     // HandleCSV csvHandling = new HandleCSV(); 
 
 
-    // // // Testing Hashing     
-    // // // //*** Generates the same hash which is good: 
-    // // //     Debug.Log(HashClass.toSHA256("Hello"));
-    // // //     Debug.Log(HashClass.toSHA256("Hello"));
+//     // void Start()
+//     // {
+//     // }
 
-// private IEnumerator test(){
+//     //     Debug.Log("count of HandleCSV's internal list ... " + csvHandling.returnCurrentCSV().Count);
+//     // {
+//     // // Tetsing CSV handling     
+//     //     // csvHandling.appendToCSV(); 
+//     //     // textField.text = testText;
+//     //     List<CSVInfo> readInInfo = csvHandling.readCSV(); 
+//     //     // string combindedString = string.Join( ",", readInInfo);
+//     //     // testText.Equals(combindedString); 
+//     //     // textField.text = testText; 
+//     //      foreach (CSVInfo z in readInInfo){
+//     //         testText = z.printCSVInfo(); 
+//     //     }
+//     //     textField.text = testText; 
 
-// }
+//     //     // Debug.Log("TEST" + readInInfo.Count); 
+//     //     CSVInfo test = new CSVInfo("25"); 
+//     //     Debug.Log(test.printCSVInfo()); 
 
-// private void Update() {
+//     //     Debug.Log("FROM TEST.CS " + HashClass.toSHA256("1111"));
+
+
+//     // // // Testing Hashing     
+//     // // // //*** Generates the same hash which is good: 
+//     // // //     Debug.Log(HashClass.toSHA256("Hello"));
+//     // // //     Debug.Log(HashClass.toSHA256("Hello"));
+
+// // private IEnumerator test(){
+
+// // }
+
+// // private void Update() {
     
-//         Debug.Log("count of HandleCSV's internal list ... " + HandleCSV.currentCSV.Count);
-// }
-//     }
+// //         Debug.Log("count of HandleCSV's internal list ... " + HandleCSV.currentCSV.Count);
+// // }
+// //     }
 
-// }
+// // }
     
+// // }
 // }
-}
